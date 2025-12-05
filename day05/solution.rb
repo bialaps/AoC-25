6f6b6c74879f5e4ea61353166ff08da7c033bb2a
Marcel Bialas <mb@zammad.com>
2025-12-05 23:42:58 +0100

    finished day05

--- day05/solution.rb
+++ day05/solution.rb
@@ -1,5 +1,50 @@
-class Day5 
+class Day5
   def self.part1
-    3
+    input = File.read(File.join(__dir__, "input.txt"))
+    
+    fresh_ids, available_ids = input.strip.split("\n\n").map(&:strip)
+
+    available_ids = available_ids.lines.map(&:to_i)
+    ranges = fresh_ids.lines.map do |line|
+      min, max = line.split("-").map(&:to_i)
+      min..max
+    end
+
+    fresh_count = available_ids.count do |id|
+      ranges.any? { |range| range.include?(id) }
+    end
+
+    fresh_count
+  end
+
+  def self.part2
+    input = File.read(File.join(__dir__, "input.txt"))
+    
+    fresh_ids, available_ids = input.strip.split("\n\n").map(&:strip)
+
+    available_ids = available_ids.lines.map(&:to_i)
+    ranges = fresh_ids.lines.map do |line|
+      min, max = line.split("-").map(&:to_i)
+      min..max
+    end
+
+    sorted = ranges.sort_by(&:begin)
+    merged = []
+
+    sorted.each do |range|
+      if merged.empty? || range.begin > merged.last.end 
+        merged << range
+      else 
+        merged[-1] = merged.last.begin..[merged.last.end, range.end].max
+      end
+    end
+
+    count = 0
+
+    merged.each do |range|
+      count += range.size
+    end
+
+    count
   end
 end
--- spec/day05_spec.rb
+++ spec/day05_spec.rb
@@ -4,7 +4,13 @@ require_relative "./../day05/solution"
 RSpec.describe Day5 do
   context "Part1" do
     it "returns the correct result" do
-      expect(Day5.part1).to eq(3)
+      expect(Day5.part1).to eq(701)
+    end
+  end
+
+  context "Part2" do 
+    it "returns the correct result" do
+      expect(Day5.part2).to eq(352340558684863)
     end
   end
 end
